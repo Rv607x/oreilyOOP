1889b197924a96b1eab898431129337944594b09
Rv607x <robah@gmail.com>
2023-01-04 16:10:56 +0300

    implementing properties of a class

---
+++ innitialize_objects.py
@@ -0,0 +1,26 @@
+class Employee:
+    # id, salary and department are class properties
+    ID = None
+    salary = None
+    department = None
+
+Steve = Employee()
+
+# to access properties of object the dot notation is used
+# example is object.property
+
+#printing properties of Steve an object of Employee
+print("ID =", Steve.ID)
+print("Salary =", Steve.salary)
+print("Department =", Steve.department)
+
+# we can also create properties outside of a class 
+# this is by assigning values to the object
+
+Steve.ID = 3789
+Steve.salary = 60000
+Steve.department = "HR"
+
+print("ID =", Steve.ID)
+print("Salary =", Steve.salary)
+print("Department =", Steve.department)
