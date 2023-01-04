19bd3ea3fea5b49f178d5406481bb63ce57f0234
Rv607x <robah@gmail.com>
2023-01-04 17:27:08 +0300

    adding space between printing of the two examples to make output look neat

--- instance_variables.py
+++ instance_variables.py
@@ -14,6 +14,7 @@ p2.formerTeams.append("PSV")
 print("Player Name: ", p1.name)
 print("Team Name: ", p1.teamName)
 print("Former Teams: ",p1.formerTeams)
+print("")
 
 print("Player Name: ", p2.name)
 print("Team Name: ", p2.teamName)
