d804ced5e53934947c53271886647a67d65abced
Rv607x <robah@gmail.com>
2023-01-04 17:20:56 +0300

    difference between class and instance variables

---
+++ instance_variables.py
@@ -0,0 +1,20 @@
+class Player:
+    teamName = "Liverpool"  # class variable
+    def __init__(self, name):
+        self.name = name  #instance variable
+        self.formerTeams = []
+
+p1 = Player("Ronaldo")
+p1.formerTeams.append("Manchester United")
+p1.formerTeams.append("Juventus")
+
+p2 = Player("Gakpo")
+p2.formerTeams.append("PSV")
+
+print("Player Name: ", p1.name)
+print("Team Name: ", p1.teamName)
+print("Former Teams: ",p1.formerTeams)
+
+print("Player Name: ", p2.name)
+print("Team Name: ", p2.teamName)
+print("Former Teams: ", p2.formerTeams)
