3b31060a4028c4795d511b99c8ed79d8e9ca4294
Rv607x <robah@gmail.com>
2023-01-05 23:20:16 +0300

    implemented a static method

--- class_methods.py
+++ class_methods.py
@@ -21,3 +21,32 @@ class Player:
         return cls.teamName
 
 print(Player.getTeamName())
+
+print("\n")
+
+
+"""Static methods are methods that are usually limited to class only and not 
+their objects. They have no direct relation 
+to class variables or instance variables."""
+
+"""
+class MyClass:
+
+    @staticmethod
+    def demo()
+        print("I am a static method")
+"""
+
+class Player2:
+    teamName = "Liverpool" #class variable
+
+    def __init__(self, name):
+        self.name = name
+
+    @staticmethod
+    def demo():
+        print("I am a static method")
+
+p1 = Player2("lol")
+p1.demo()
+Player2.demo()
